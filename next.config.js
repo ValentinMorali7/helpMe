9be92a34bab2fc8d24c9665368d1a8581edff5bc
davidmarco12 <davidmarcolin@sirius.com.ar>
2024-09-23 15:34:33 -0300

    build nextjs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {}
 // module.exports = nextConfig
 module.exports = {
     output: "standalone",
+    reactStrictMode: true,
+    distDir: 'build',
     async redirects() {
         return [
             {
