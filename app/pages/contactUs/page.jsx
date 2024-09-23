8773f228c65d07d9db8ddc4eb18feb9d96a5299a
Valentin Morali <valentinmorali@sirius.com.ar>
2024-09-23 15:34:32 -0300

    contactus page

--- app/pages/contactUs/page.jsx
+++ app/pages/contactUs/page.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React from 'react'
+import {
+    FacebookLogo,
+    InstagramLogo,
+    TwitterLogo,
+} from '../../../components/styledIcons'
+import { title } from '../../../components/primitives'
 
 // asd
 const ContactUs = () => {
-  return (
-    <div >
-      <ul >
-        <li >
-          <strong>ContactUs page</strong>
-        </li>
+    return (
+        <>
+            <div className="flex flex-row justify-center ">
+                <span className={title()}>Somos&nbsp;</span>
+                <span className={title({ color: 'blue' })}>HelpMe!&nbsp;</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mt-20">
+                <a href="https://instagram.com">
+                    <InstagramLogo size={150} />
+                </a>
+                <a href="https://facebook.com">
+                    <FacebookLogo size={150} />
+                </a>
+                <a href="https://twitter.com">
+                    <TwitterLogo size={150} />
+                </a>
+            </div>
+            <div className="flex flex-row justify-center mt-20">
+                <p className="text-lg">
+                    Para más información, contactanos por mail:&nbsp;
+                </p>
+                <a className="text-cyan-600 text-lg" href="www.gmail.com">
+                    soporte@helpme.com
+                </a>
+            </div>
+        </>
+    )
+}
 
-      </ul>
-    </div>
-  );
-};
-
-export default ContactUs;
+export default ContactUs
