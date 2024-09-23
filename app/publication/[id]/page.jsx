e33728caa0d8592890c8973643942cfe6eb78c24
davidmarco12 <davidmarcolin@sirius.com.ar>
2024-09-23 15:14:53 -0300

    disable eslint

--- app/publication/[id]/page.jsx
+++ app/publication/[id]/page.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 'use client'
 import React, { useEffect, useState } from 'react'
 import {
